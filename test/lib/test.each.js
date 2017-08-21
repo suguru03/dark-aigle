75eb80057e492940670dbe53185e2cf38e06f0e7
Suguru Motegi <suguru.motegi@gmail.com>
2017-08-21 15:19:46 -0700

    feat(each): add an each class function

--- test/lib/test.each.js
+++ test/lib/test.each.js
@@ -52,3 +52,49 @@ describe('each', () => {
     ]);
   });
 });
+
+describe('#each', () => {
+
+  it('should execute in parallel', async () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key, coll) => {
+      assert.strictEqual(coll, collection);
+      return new Promise(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value);
+      }, DELAY * value));
+    };
+    const res = await Promise.resolve(collection).each(iterator);
+    assert.strictEqual(res, undefined);
+    assert.deepEqual(order, [
+      [0, 1],
+      [2, 2],
+      [1, 4]
+    ]);
+  });
+
+  it('should execute with object collection in parallel', async () => {
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2
+    };
+    const iterator = (value, key, coll) => {
+      assert.strictEqual(coll, collection);
+      return new Promise(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value);
+      }, DELAY * value));
+    };
+    const res = await Promise.resolve(collection).each(iterator);
+    assert.strictEqual(res, undefined);
+    assert.deepEqual(order, [
+      ['task1', 1],
+      ['task3', 2],
+      ['task2', 4]
+    ]);
+  });
+});
