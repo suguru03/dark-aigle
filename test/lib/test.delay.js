8d27a4246afc90974368ae432f7e67cfdbf5c64c
Suguru Motegi <suguru.motegi@gmail.com>
2017-08-30 16:18:58 -0700

    feat(delay): add delay

---
+++ test/lib/test.delay.js
@@ -0,0 +1,47 @@
+'use strict';
+
+require('../..');
+
+const assert = require('assert');
+
+const parallel = require('mocha.parallel');
+
+const { DELAY } = require('../config');
+
+parallel('delay', () => {
+
+  it('should delay', async () => {
+    const start = Date.now();
+    await Promise.delay(DELAY);
+    const diff = Date.now() - start + 1;
+    assert.ok(diff >= DELAY);
+  });
+
+  it('should pass a value', async () => {
+    const value = {};
+    const start = Date.now();
+    const res = await Promise.delay(DELAY, value);
+    const diff = Date.now() - start + 1;
+    assert.ok(diff >= DELAY);
+    assert.strictEqual(res, value);
+  });
+});
+
+parallel('#delay', () => {
+
+  it('should delay', async () => {
+    const start = Date.now();
+    await Promise.resolve().delay(DELAY);
+    const diff = Date.now() - start + 1;
+    assert.ok(diff >= DELAY);
+  });
+
+  it('should pass a previous value', async () => {
+    const value = {};
+    const start = Date.now();
+    const res = await Promise.resolve(value).delay(DELAY);
+    const diff = Date.now() - start + 1;
+    assert.ok(diff >= DELAY);
+    assert.strictEqual(res, value);
+  });
+});
