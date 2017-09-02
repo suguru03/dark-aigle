9631bbf036a23dd8bc0d507c43111c4f01753f58
Suguru Motegi <suguru.motegi@gmail.com>
2017-09-02 16:35:57 -0700

    feat(mapSeries): add mapSeries

---
+++ test/lib/test.mapSeries.js
@@ -0,0 +1,192 @@
+'use strict';
+
+require('../..');
+
+const assert = require('assert');
+
+const parallel = require('mocha.parallel');
+
+const { DELAY } = require('../config');
+
+parallel('mapSeries', () => {
+
+  it('should execute in series', async () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Promise(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value * 2);
+      }, DELAY * value));
+    };
+    const res = await Promise.mapSeries(collection, iterator);
+    assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
+    assert.deepEqual(res, [2, 8, 4]);
+    assert.deepEqual(order, [
+      [0, 1],
+      [1, 4],
+      [2, 2]
+    ]);
+  });
+
+  it('should execute on synchronous', async () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => value * 2;
+    const res = await Promise.mapSeries(collection, iterator);
+    assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
+    assert.deepEqual(res, [2, 8, 4]);
+  });
+
+  it('should execute with object collection in series', async () => {
+
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2
+    };
+    const iterator = (value, key) => {
+      return new Promise(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value * 2);
+      }, DELAY * value));
+    };
+    const res = await Promise.mapSeries(collection, iterator);
+    assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
+    assert.deepEqual(res, [2, 8, 4]);
+    assert.deepEqual(order, [
+      ['task1', 1],
+      ['task2', 4],
+      ['task3', 2]
+    ]);
+  });
+
+  it('should return an empty array if collection is an empty array', async () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    const res = await Promise.mapSeries([], iterator);
+    assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
+    assert.strictEqual(res.length, 0);
+  });
+
+  it('should return an empty array if collection is an empty object', async () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    const res = await Promise.mapSeries({}, iterator);
+    assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
+    assert.strictEqual(res.length, 0);
+  });
+
+  it('should return an empty array if collection is string', async () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    const res = await Promise.mapSeries('test', iterator);
+    assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
+    assert.strictEqual(res.length, 0);
+  });
+
+  it('should catch a TypeError', async () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    try {
+      await Promise.mapSeries(collection, iterator);
+      assert.fail();
+    } catch (e) {
+      assert.ok(e instanceof TypeError);
+    }
+  });
+
+  it('should execute even if arguments are not exists', () => {
+
+    return Promise.mapSeries();
+  });
+});
+
+parallel('#mapSeries', () => {
+
+  it('should execute in series', async () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Promise(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value * 2);
+      }, DELAY * value));
+    };
+    const res = await Promise.resolve(collection).mapSeries(iterator);
+    assert.deepEqual(res, [2, 8, 4]);
+    assert.deepEqual(order, [
+      [0, 1],
+      [1, 4],
+      [2, 2]
+    ]);
+  });
+
+  it('should execute with object collection in series', async () => {
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2
+    };
+    const iterator = (value, key) => {
+      return new Promise(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value * 2);
+      }, DELAY * value));
+    };
+    const res = await Promise.resolve(collection).mapSeries(iterator);
+    assert.deepEqual(res, [2, 8, 4]);
+    assert.deepEqual(order, [
+      ['task1', 1],
+      ['task2', 4],
+      ['task3', 2]
+    ]);
+  });
+
+  it('should execute with delay', async () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Promise(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value * 2);
+      }, DELAY * value));
+    };
+    const res = await Promise.delay(DELAY, collection).mapSeries(iterator);
+    assert.deepEqual(res, [2, 8, 4]);
+    assert.deepEqual(order, [
+      [0, 1],
+      [1, 4],
+      [2, 2]
+    ]);
+  });
+
+  it('should catch a TypeError', async () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    try {
+      await Promise.resolve(collection).mapSeries(iterator);
+      assert.fail();
+    } catch (e) {
+      assert.ok(e instanceof TypeError);
+    }
+  });
+});
+
