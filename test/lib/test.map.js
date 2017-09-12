92cd2a9216c218b05730b029ad7f6968fe321b1e
Suguru Motegi <suguru.motegi@gmail.com>
2017-09-11 18:36:43 -0700

    refactor(map): refactor map

--- test/lib/test.map.js
+++ test/lib/test.map.js
@@ -10,18 +10,18 @@ const { DELAY } = require('../config');
 
 parallel('map', () => {
 
-  it('should work in parallel', async () => {
+  it('should execute in parallel', async () => {
 
     const order = [];
     const collection = [1, 4, 2];
-    const iterator = (value, key, coll) => {
-      assert.strictEqual(coll, collection);
+    const iterator = (value, key) => {
       return new Promise(resolve => setTimeout(() => {
         order.push([key, value]);
         resolve(value * 2);
       }, DELAY * value));
     };
     const res = await Promise.map(collection, iterator);
+    assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
     assert.deepEqual(res, [2, 8, 4]);
     assert.deepEqual(order, [
       [0, 1],
@@ -30,7 +30,7 @@ parallel('map', () => {
     ]);
   });
 
-  it('should work with object', async () => {
+  it('should execute with object collection in parallel', async () => {
 
     const order = [];
     const collection = {
@@ -38,20 +38,64 @@ parallel('map', () => {
       task2: 4,
       task3: 2
     };
-    const iterator = (value, key, coll) => {
-      assert.strictEqual(coll, collection);
+    const iterator = (value, key) => {
       return new Promise(resolve => setTimeout(() => {
         order.push([key, value]);
         resolve(value * 2);
       }, DELAY * value));
     };
     const res = await Promise.map(collection, iterator);
+    assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
     assert.deepEqual(res, [2, 8, 4]);
     assert.deepEqual(order, [
       ['task1', 1],
       ['task3', 2],
       ['task2', 4]
     ]);
+  });
+
+  it('should return an empty array if collection is an empty array', async () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    const res = await Promise.map([], iterator);
+    assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
+    assert.strictEqual(res.length, 0);
+  });
+
+  it('should return an empty array if collection is an empty object', async () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    const res = await Promise.map({}, iterator);
+    assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
+    assert.strictEqual(res.length, 0);
+  });
+
+  it('should return an empty array if collection is string', async () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    const res = await Promise.map('test', iterator);
+    assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
+    assert.strictEqual(res.length, 0);
+  });
+
+  it('should catch a TypeError', async () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    try {
+      await Promise.map(collection, iterator);
+      assert.fail();
+    } catch (e) {
+      assert.ok(e instanceof TypeError);
+    }
   });
 });
 
@@ -61,8 +105,7 @@ parallel('#map', () => {
 
     const order = [];
     const collection = [1, 4, 2];
-    const iterator = (value, key, coll) => {
-      assert.strictEqual(coll, collection);
+    const iterator = (value, key) => {
       return new Promise(resolve => setTimeout(() => {
         order.push([key, value]);
         resolve(value * 2);
@@ -84,8 +127,7 @@ parallel('#map', () => {
       task2: 4,
       task3: 2
     };
-    const iterator = (value, key, coll) => {
-      assert.strictEqual(coll, collection);
+    const iterator = (value, key) => {
       return new Promise(resolve => setTimeout(() => {
         order.push([key, value]);
         resolve(value * 2);
@@ -99,4 +141,37 @@ parallel('#map', () => {
       ['task2', 4]
     ]);
   });
+
+  it('should execute with delay', async () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Promise(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value * 2);
+      }, DELAY * value));
+    };
+    const res = await Promise.delay(DELAY, collection).map(iterator);
+    assert.deepEqual(res, [2, 8, 4]);
+    assert.deepEqual(order, [
+      [0, 1],
+      [2, 2],
+      [1, 4]
+    ]);
+  });
+
+  it('should catch a TypeError', async () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    try {
+      await Promise.resolve(collection).map(iterator);
+    } catch (e) {
+      assert.ok(e instanceof TypeError);
+    }
+  });
 });
+
