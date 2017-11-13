9a1a20a1de5d5d5f9ca1a5087c1d4535c9066ebe
Suguru Motegi <suguru.motegi@gmail.com>
2017-11-12 16:42:56 -0800

    test(aiglify): add a test case to check missing functions

--- test/lib/test.aiglify.js
+++ test/lib/test.aiglify.js
@@ -1,7 +1,10 @@
 'use strict';
 
+require('..');
+
 const assert = require('assert');
 
+const Aigle = require('aigle');
 const parallel = require('mocha.parallel');
 
 const aiglify = require('../../lib/aiglify');
@@ -21,6 +24,25 @@ parallel('aiglify', () => {
     }
     aiglify(P);
     assert.strictEqual(typeof P.delay, 'function');
+  });
+
+  it.skip('should have aigle functions', () => {
+
+    const errors = [];
+    Object.getOwnPropertyNames(Aigle)
+      .sort()
+      .forEach(key => {
+        try {
+          assert.ok(Promise[key]);
+        } catch (e) {
+          errors.push(key);
+        }
+      });
+    if (!errors.length) {
+      return;
+    }
+    errors.unshift('');
+    assert.fail(errors.join('\n\t'));
   });
 
 });
